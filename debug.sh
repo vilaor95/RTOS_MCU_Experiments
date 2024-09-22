55bcacb1320ba9ab5ddac9396bb5678c16547602
vlo <vilaor@teleco.upv.es>
2024-09-22 23:20:10 +0200

    st-util connect under reset
    don´t execute code until debugger is connected, that way always start
    debugging at the first code instruction

--- debug.sh
+++ debug.sh
@@ -4,6 +4,6 @@ source ./project.sh
 
 make out.bin && \
 	st-flash write out.bin 0x8000000 && \
-	st-util &
+	st-util -u &
 ${TOOLCHAIN_PREFIX}gdb out.elf -ex 'target extended-remote localhost:4242'
 killall st-util
