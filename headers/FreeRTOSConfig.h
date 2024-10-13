e08d3960e6b9a99cd68899a9f1b1488ba0b56bb1
vlo <vilaor@teleco.upv.es>
2024-10-13 19:12:22 +0200

    set more prio interrupt that can use RTOS API calls
    Taking in account that CortexM priorities go in inversed order!
    Interrupts with lower number (higher prio) than 7 can NOT use RTOS API
    calls. Interrupts with higher number (lower prio) or equal to 7 can use
    RTOS API calls.
    
    Set priority of the button IRQ to 14, just to be have more priority that
    the tick interrupt.

--- headers/FreeRTOSConfig.h
+++ headers/FreeRTOSConfig.h
@@ -313,7 +313,7 @@
  * switch performing interrupts.  Not supported by all FreeRTOS ports.  See
  * https://www.freertos.org/RTOS-Cortex-M3-M4.html for information specific to
  * ARM Cortex-M devices. */
-#define configKERNEL_INTERRUPT_PRIORITY          0
+#define configKERNEL_INTERRUPT_PRIORITY          (15)
 
 /* configMAX_SYSCALL_INTERRUPT_PRIORITY sets the interrupt priority above which
  * FreeRTOS API calls must not be made.  Interrupts above this priority are
@@ -321,7 +321,7 @@
  * to the highest interrupt priority (0).  Not supported by all FreeRTOS ports.
  * See https://www.freertos.org/RTOS-Cortex-M3-M4.html for information specific
  * to ARM Cortex-M devices. */
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY     (0xf0)
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY     (0x70)
 
 /* Another name for configMAX_SYSCALL_INTERRUPT_PRIORITY - the name used depends
  * on the FreeRTOS port. */
--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ void SystemInit(void)
 	set_bit((uint32_t*)&EXTI->IMR, 13); // Set interrupt mask register
 	set_bit((uint32_t*)&EXTI->RTSR, 13); // Set rising trigger
 	clear_bit((uint32_t*)&EXTI->FTSR, 13); // Clear falling trigger
-	NVIC_SetPriority(EXTI15_10_IRQn, 15);
+	NVIC_SetPriority(EXTI15_10_IRQn, 14);
 	NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 	watchdog_start();
